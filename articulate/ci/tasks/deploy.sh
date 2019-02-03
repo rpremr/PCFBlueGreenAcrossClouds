42a304e12152d49404e8892b1ab44d0b8e238603
Ahilan Ponnusamy <ahilanp@gmail.com>
2019-02-02 22:02:29 -0600

    commits

---
+++ articulate/ci/tasks/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set +e
+
+cf api ${api} --skip-ssl-validation
+cf login -u ${username} -p ${password} -o ${organization} -s ${space}
+
+cf apps | grep blue-${app_name}
+
+if [ $? -eq 0 ]
+then
+  echo "blue Available deploying green"
+
+  cf push green-${app_name} -p ../../target/articulate-1.0.0.jar -m 800m -n green-${app_name}
+  cf map-route green-${app_name} -${domain} --hostname ${app_host}
+  cf unmap-route green-ut-articulate ${domain} --hostname green-${app_host}
+
+  cf delete blue-${app_name} -f
+  cf delete-route ${domain} --hostname blue-${app_name} -f
+  cf delete-route ${domain} --hostname green-${app_name} -f
+else
+  echo "blue not available deploying blue"
+
+  cf push blue-${app_name} -p ../../target/articulate-1.0.0.jar -m 800m -n blue-${app_name}
+  cf map-route blue-${app_name} ${domain} --hostname ${app_host}
+  cf unmap-route blue-${app_name} ${domain} --hostname blue-${app_host}
+  
+  cf delete green-${app_name} -f
+  cf delete-route ${domain} --hostname green-${app_host} -f
+  cf delete-route ${domain} --hostname blue-${app_host} -f
+fi
