910c7c73912a686f6eb0d26b016c73bc38808277
Prem Raghav <prem@prems-mbp.lan>
2019-03-29 14:07:22 -0500

    Commit Changes....

--- git-commit.sh
+++ git-commit.sh
@@ -2,4 +2,4 @@
 
 git add .
 git commit -m "Commit Changes...."
-git push https://github.com/AhilanPonnusamy/pcf101-articulate-attendee.git/
+git push https://github.com/rpremr/PCFBlueGreenAcrossClouds.git/
